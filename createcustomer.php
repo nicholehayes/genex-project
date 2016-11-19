7458976c746da08cf299bb40a55f88be2caa9da3
Amy Guitreau <amyguitreau@gmail.com>
2016-11-18 22:08:02 -0600

    fixed createcustomer.php location

--- createcustomer.php
+++ createcustomer.php
@@ -1,6 +1,6 @@
 <?php
 include_once 'dbconnect.php';
-if(isset($_GET["createcustomer"])) {
+if(isset($_POST["createcustomer"])) {
         $fname = mysql_real_escape_string($_POST['firstname']);
         $lname = mysql_real_escape_string($_POST['lastname']);
         $telnum = mysql_real_escape_string($_POST['telnum']);
@@ -9,12 +9,20 @@ if(isset($_GET["createcustomer"])) {
 	$state = mysql_real_escape_string($_POST['state']);
 	$street = mysql_real_escape_string($_POST['street']);
         $zip = mysql_real_escape_string($_POST['zipcode']);
-        if(mysql_query("INSERT INTO customer(first_name,last_name,street,city,state,zipcode,phone_number,email) VALUES('$fname','$lname','$street','$city','$state', $zip, $telnum, '$email')"))
+        if(mysql_query("INSERT INTO customer (first_name,last_name,street,city,state,zipcode,phone_number,email) VALUES('$fname','$lname','$street','$city','$state', $zip, $telnum, '$email')"))
         {
                 echo "success";
         }else
         {
                 echo "failed";
+		echo "$fname\n";
+ echo "$lname\n";
+ echo "$street\n";
+ echo "$city\n";
+ echo "$state\n";
+ echo "$zip\n";
+ echo "$telnum\n";
+ echo "$email\n";
 		
         }
 
