a3b1dc8a14f455cf20a19bbc32bc81a43d95ac51
Ryan Ngo <rawrimryno@gmail.com>
2016-11-19 15:24:46 -0600

    no message

--- dev/js/registerCustomer.js
+++ dev/js/registerCustomer.js
@@ -1,16 +1,17 @@
 function registerCustomer(){
+	var formData = JSON.stringify($("#registercustomerform").serializeArray());
 	$.ajax({
-		url: './php/createcustomer.php',
-		data: $('#registercustomerform').serialize(),
 		type: 'POST',
+		url: 'http://continentalgenetics.ddns.net:8080/add_customer',
+		data: formData,
+		dataType: 'json',
+		contentType:"application/json",
 		success:function(data){
 			if(~data.indexOf("1")){
 				alert("success");
-				window.location.reload();
 			}
 			else{
 				alert(data);
-				window.location.reload();
 			}
 		}
 	});
