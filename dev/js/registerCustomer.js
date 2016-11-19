7f062e2707998ce17fb1e8459491f1ef9d793fdb
Ryan Ngo <rawrimryno@gmail.com>
2016-11-18 22:19:13 -0600

    registerCustomer

---
+++ dev/js/registerCustomer.js
@@ -0,0 +1,20 @@
+function registerCustomer(){
+	var form = $('#registercustomerform')[0]; // You need to use standart javascript object here
+	var formData = new FormData(form);
+	$.ajax({
+		url: '/php/createcustomer.php',
+		data: formData,
+		type: 'POST',
+		// THIS MUST BE DONE FOR FILE UPLOADING
+		contentType: false,
+		processData: false,
+		success:function(data){
+			if(data.substr(0,1) === "1"){
+				alert("success");
+			}
+			else{
+				alert(data);				
+			}
+		}
+	});
+}
