a8aee1cc3103f7f10a9a14e3400bcab6ff09ef0d
Amy Guitreau <amyguitreau@gmail.com>
2016-11-20 17:00:39 -0600

    new collection

--- dev/js/test.js
+++ dev/js/test.js
@@ -13,3 +13,16 @@ function getBreeds(){
 $(document).ready(function(){
 	getBreeds();
 });
+
+function getStud(){
+
+	var params = {pie: '01', box: '01'};
+	$.get("http://continentalgenetics.ddns.net:8080/stud/get",params, function( data ) {
+		var dat = (data)
+		var test ="<option value=''>Choose Stud</option>";
+		for (var x in dat){
+			test+="<option value='"+dat[x].stud_code+"'>"+dat[x].stud_code+"</option>";
+		}
+		$('#tank').html(test);
+	});
+}test+="<option value='"+dat[x].collection_id+"'>"+dat[x].breed_abbreviation+pad(dat[x].bull_id,5)+fixDate(dat[x].date)+"</option>";
