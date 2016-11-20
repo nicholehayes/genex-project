ffb107c9e6333dbe6ac3e3bbce96d15a1c999c11
W. Craig Jones <Sciencegeek123@hotmail.com>
2016-11-20 13:39:43 -0600

    Updated get_remaining_units and added more transaction types

--- server/scripts/location.js
+++ server/scripts/location.js
@@ -5,6 +5,7 @@
  */
 
 var helper = require('./helper');
+const qs = require('querystring');
 var server = global.server;
 
 function location_get(req, res, next) {
@@ -20,7 +21,25 @@ function location_put(req, res, next) {
 server.put('/location/put', location_put);
 
 function location_all_units(req, res, next) {
-    var sql = "SELECT location.location_id, location.tank_number, location.pie, location.box, COALESCE(SUM(storage.units), 0) AS total_units FROM location LEFT OUTER JOIN storage ON storage.location_id = location.location_id GROUP BY location.location_id ORDER BY location.location_id ASC";
-    helper.sqlWrapper(sql,[],res,next);
+    if(Object.keys(req.query).length > 0) {
+        var wherestm = "WHERE ";
+        var sel = [];
+
+        Object.keys(req.query).forEach(function(val, index, array) {
+            wherestm += qs.escape(val) + " LIKE ?";
+            sel.push(qs.escape(req.query[val]));
+            if(index + 1 != array.length) {
+                wherestm += " AND ";
+            }
+        });
+
+        var sql = "SELECT location.location_id, location.tank_number, location.pie, location.box, COALESCE(SUM(storage.units), 0) AS total_units FROM location LEFT OUTER JOIN storage ON storage.location_id = location.location_id "+
+        wherestm + " GROUP BY location.location_id ORDER BY location.location_id ASC";
+        helper.sqlWrapper(sql,sel,res,next);
+    } else {
+        var sql = "SELECT location.location_id, location.tank_number, location.pie, location.box, COALESCE(SUM(storage.units), 0) AS total_units FROM location LEFT OUTER JOIN storage ON storage.location_id = location.location_id GROUP BY location.location_id ORDER BY location.location_id ASC";
+        helper.sqlWrapper(sql,[],res,next);
+    }
 }
+server.get('/location/unit_count', location_all_units);
 server.get('/location/all_unit_count', location_all_units);
--- server/scripts/transaction.js
+++ server/scripts/transaction.js
@@ -19,83 +19,143 @@ function transaction_put(req, res, next) {
 }
 server.put('/transaction/put', transaction_put);
 
-function transaction_add(req, res, next) {
+function transaction_add_rm(req, res, next) {
     if(!req.body.trans_type) {
         res.send(400, "Missing transaction trans_type");
         return next();
-    } else {
-        if(req.body.trans_type == 1 || req.body.trans_type == 2) { //Discard
-            console.log("*Discard or Ship transaction");
-            if( !req.body.collection_id ||
-                !req.body.units ||
-                !req.body.datetime ||
-                !req.body.from_location_id) 
-            {
-                res.send(400,"Missing arguments.");
-                return next();
-            }
-
-            sql1 = "INSERT INTO transaction (collection_id, units, datetime, trans_type, from_location_id) VALUES (?,?,?,?,?)";
-            sel1 = [
-                req.body.collection_id,
-                req.body.units,
-                req.body.datetime,
-                req.body.trans_type,
-                req.body.from_location_id
-            ];
-
-            sql2 = "UPDATE storage SET units = units - ? WHERE collection_id = ? AND location_id = ?";
-            sel2 = [
-                req.body.units,
-                req.body.collection_id,
-                req.body.from_location_id
-            ]
-
-            helper.sqlWrapper(sql1,sel1, res, next, function(row, fields) {
-                helper.sqlWrapper(sql2, sel2, res, next);
-            });
-        } else {
-            console.log("*Ship transaction");
-            if( !req.body.collection_id ||
-                !req.body.units ||
-                !req.body.datetime ||
-                !req.body.from_location_id ||
-                !req.body.to_location_id)
-            {
-                res.send(400,"Missing arguments.");
-                return next();
-            }
-
-            sql1 = "INSERT INTO transaction (collection_id, units, datetime, trans_type, from_location_id, to_location_id) VALUES (?,?,?,?,?,?)";
-            sel1 = [
-                req.body.collection_id,
-                req.body.units,
-                req.body.datetime,
-                req.body.trans_type,
-                req.body.from_location_id,
-                req.body.to_location_id
-            ];
-
-            sql2 = "UPDATE storage SET units = units - ? WHERE collection_id = ? AND location_id = ?";
-            sel2 = [
-                req.body.units,
-                req.body.collection_id,
-                req.body.from_location_id
-            ]
-
-            sql3 = "UPDATE storage SET units = units + ? WHERE collection_id = ? AND location_id = ?";
-            sel3 = [
-                req.body.units,
-                req.body.collection_id,
-                req.body.to_location_id
-            ]
-
-            helper.sqlWrapper(sql1,sel1, res, next, function(row, fields) {
-                helper.sqlWrapper(sql2, sel2, res, next, function(row,fields) {
-                    helper.sqlWrapper(sql3, sel3, res, next);
-                });
-            });
-        }
     }
+
+    if(req.body.trans_type != 1 && req.body.trans_type != 2) {
+        res.send(400, "Wrong transaction type");
+        return next();
+    }
+
+    console.log("*Discard or Ship transaction");
+    if( !req.body.collection_id ||
+        !req.body.units ||
+        !req.body.datetime ||
+        !req.body.from_location_id) 
+    {
+        res.send(400,"Missing arguments.");
+        return next();
+    }
+
+    sql1 = "INSERT INTO transaction (collection_id, units, datetime, trans_type, from_location_id) VALUES (?,?,?,?,?)";
+    sel1 = [
+        req.body.collection_id,
+        req.body.units,
+        req.body.datetime,
+        req.body.trans_type,
+        req.body.from_location_id
+    ];
+
+    sql2 = "UPDATE storage SET units = units - ? WHERE collection_id = ? AND location_id = ?";
+    sel2 = [
+        req.body.units,
+        req.body.collection_id,
+        req.body.from_location_id
+    ]
+
+    helper.sqlWrapper(sql1,sel1, res, next, function(row, fields) {
+        helper.sqlWrapper(sql2, sel2, res, next);
+    });
 }
-server.post('/transaction/add', transaction_add);
+server.post('/transaction/add/ship', transaction_add_rm);
+server.post('/transaction/add/delete', transaction_add_rm);
+
+function transaction_add_mv(req, res, next) {
+    if(!req.body.trans_type) {
+        res.send(400, "Missing transaction trans_type");
+        return next();
+    }
+
+    if(req.body.trans_type != 3) {
+        res.send(400, "Wrong transaction type");
+        return next();
+    }
+
+    console.log("*Ship transaction");
+    if( !req.body.collection_id ||
+        !req.body.units ||
+        !req.body.datetime ||
+        !req.body.from_location_id ||
+        !req.body.to_location_id)
+    {
+        res.send(400,"Missing arguments.");
+        return next();
+    }
+
+    sql1 = "INSERT INTO transaction (collection_id, units, datetime, trans_type, from_location_id, to_location_id) VALUES (?,?,?,?,?,?)";
+    sel1 = [
+        req.body.collection_id,
+        req.body.units,
+        req.body.datetime,
+        req.body.trans_type,
+        req.body.from_location_id,
+        req.body.to_location_id
+    ];
+
+    sql2 = "UPDATE storage SET units = units - ? WHERE collection_id = ? AND location_id = ?";
+    sel2 = [
+        req.body.units,
+        req.body.collection_id,
+        req.body.from_location_id
+    ]
+
+    sql3 = "UPDATE storage SET units = units + ? WHERE collection_id = ? AND location_id = ?";
+    sel3 = [
+        req.body.units,
+        req.body.collection_id,
+        req.body.to_location_id
+    ]
+
+    helper.sqlWrapper(sql1,sel1, res, next, function(row, fields) {
+        helper.sqlWrapper(sql2, sel2, res, next, function(row,fields) {
+            helper.sqlWrapper(sql3, sel3, res, next);
+        });
+    });
+}
+server.post('/transaction/add/move', transaction_add_mv);
+
+function transaction_add_in(req, res, next) {
+    if(!req.body.trans_type) {
+        res.send(400, "Missing transaction trans_type");
+        return next();
+    }
+
+    if(req.body.trans_type != 1 && req.body.trans_type != 2) {
+        res.send(400, "Wrong transaction type");
+        return next();
+    }
+
+    console.log("*Discard or Ship transaction");
+    if( !req.body.collection_id ||
+        !req.body.units ||
+        !req.body.datetime ||
+        !req.body.to_location_id) 
+    {
+        res.send(400,"Missing arguments.");
+        return next();
+    }
+
+    sql1 = "INSERT INTO transaction (collection_id, units, datetime, trans_type, to_location_id) VALUES (?,?,?,?,?)";
+    sel1 = [
+        req.body.collection_id,
+        req.body.units,
+        req.body.datetime,
+        req.body.trans_type,
+        req.body.to_location_id
+    ];
+
+    sql2 = "UPDATE storage SET units = units + ? WHERE collection_id = ? AND location_id = ?";
+    sel2 = [
+        req.body.units,
+        req.body.collection_id,
+        req.body.to_location_id
+    ]
+
+    helper.sqlWrapper(sql1,sel1, res, next, function(row, fields) {
+        helper.sqlWrapper(sql2, sel2, res, next);
+    });
+}
+server.post('/transaction/add/insert', transaction_add_in);
