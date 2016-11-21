6d17534d500c8caf771e24af188e66798a0121fd
W. Craig Jones <wcj@cjones.io>
2016-11-20 19:41:43 -0600

    updated transaction

--- dev/js/newTransaction.js
+++ dev/js/newTransaction.js
@@ -28,10 +28,64 @@ function getTransactions() {
             var boxloc = data['box'];
             var boxUnits = data['units'];
 
-            var row = " <tr><td class=\"mdl-data-table__cell--non-numeric\"></td>"+collectionID+" <td>"+tankloc+"</td> <td>"+pieloc+"</td> <td>"+boxloc+"</td> <td>"+boxUnits+"</td> </tr>";
+            var row = " <tr><td class=\"mdl-data-table__cell--non-numeric\">"+collectionID+"</td> <td>"+tankloc+"</td> <td>"+pieloc+"</td> <td>"+boxloc+"</td> <td>"+boxUnits+"</td> </tr>";
             $("#tableData").append(row);
         });
 	});
+}
+
+function getTank(){
+	var params = {pie: '01', box: '01'};
+	$.get("http://continentalgenetics.ddns.net:8080/location/get",params, function( data ) {
+		var dat = (data)
+		var test ="<option value=''>Choose Tank</option>";
+		for (var x in dat){
+			test+="<option value='"+dat[x].tank_number+"'>"+dat[x].tank_number+"</option>";
+		}
+		$('#tank').html(test);
+	});
+}
+
+function changeTank(){
+	$('#tanknum').val($('#tank').find(':selected').val()).parent().addClass('is-focused');
+	var tank = $('#tank').find(':selected').val();
+	if (tank!= null || tank != '') {
+		$.get("http://continentalgenetics.ddns.net:8080/location/unit_count", {tank_number: tank}, function (data) {
+			var dat = (data)
+			var test = "";
+			$('.box-card').removeClass('box-green');
+			$('.box-card').removeClass('box-yellow');
+			$('.box-card').removeClass('box-orange');
+			$('.box-card').removeClass('box-red');
+
+			for (var x in dat) {
+				var pie = dat[x].pie;
+				var box = dat[x].box;
+				var id = pie + box;
+				$("#" + id).text(dat[x].total_units);
+				
+				if($("#" + id).text() < 100){
+
+					$('#'+ id).addClass('box-green');
+				}
+				else if($("#" + id).text() < 200){
+
+					$("#" + id).addClass('box-yellow');
+				}
+				else if($("#" + id).text() < 300){
+
+					$('#'+ id).addClass('box-orange');
+				}
+				else{
+
+					$('#'+ id).addClass('box-red');
+				}
+			}
+		});
+	}
+	componentHandler.upgradeDom();
+	//return getColor();
+
 }
 
 $(document).ready(function(){
